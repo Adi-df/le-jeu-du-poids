93607ae1e53052620f123ea786259a788452d716
Adi-df <adidf-web@laposte.net>
2022-04-03 11:22:37 +0200

    Draw players list

--- pages/play.tsx
+++ pages/play.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from "next"
+import { useState } from "react"
+import PlayerInput from "../components/PlayerInput"
 import homeStyles from "../styles/Home.module.css"
 import styles from "../styles/Play.module.css"
 
 const Play: NextPage = () => {
+  const [players, setPlayer] = useState<{ name: string }[]>([]);
+
   return <main className={homeStyles.main}>
     <div className={homeStyles.title}>
       <h1>Jouer !</h1>
     </div>
     <div className={styles.form}>
+      {players.map(({ name }, i) => <PlayerInput name={name} key={i} />)}
     </div>
   </main>
 }
