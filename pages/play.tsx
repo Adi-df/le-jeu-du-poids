e1e1cad83f29b6755d0f559ee8c116815df8d5f0
Adi-df <adidf-web@laposte.net>
2022-04-03 10:56:31 +0200

    Add player count input

--- pages/play.tsx
+++ pages/play.tsx
@@ -1,14 +1,19 @@
 import type { NextPage } from "next"
-import styles from "../styles/Home.module.css"
+import homeStyles from "../styles/Home.module.css"
+import styles from "../styles/Play.module.css"
 
 const Play: NextPage = () => {
-  return <div>
-    <main className={styles.main}>
-      <div className={styles.title}>
-        <h1>Jouer !</h1>
+  return <main className={homeStyles.main}>
+    <div className={homeStyles.title}>
+      <h1>Jouer !</h1>
+    </div>
+    <div className={styles.form}>
+      <div className={styles.count_input}>
+        <label>Nombre de joueurs : </label>
+        <input type={"number"} min={2} />
       </div>
-    </main>
-  </div>
+    </div>
+  </main>
 }
 
 export default Play;
