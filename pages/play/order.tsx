1e0560886999f6ffeff9236254ca12621e30b324
Adi-df <adidf-web@laposte.net>
2022-04-03 15:57:12 +0200

    Show sorted players

---
+++ pages/play/order.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next"
+import { useStore } from "../../store/store"
+import styles from "../../styles/Order.module.css"
+
+const Round: NextPage = () => {
+  const { players, targetWeight } = useStore(({ players, targetWeight }) => ({ players, targetWeight }))
+
+  const sortedPlayers = players.map(player => ({ dist: Math.abs((targetWeight || 0) - player.weight), player })).sort((p1, p2) => p1.dist - p2.dist).map(({ player }) => player);
+
+  return <div>
+    <div>
+      {sortedPlayers.map((player, i) => <div>
+        <div>{i + 1}</div>
+        <div>{player.name}</div>
+      </div>)}
+    </div>
+  </div>
+}
+
+export default Round
