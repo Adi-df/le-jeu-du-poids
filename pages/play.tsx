190cb9a74d647ad94abb664dafdb429113ab2d33
Adi-df <adidf-web@laposte.net>
2022-04-03 10:50:31 +0200

    Create Play page

---
+++ pages/play.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next"
+import styles from "../styles/Home.module.css"
+
+const Play: NextPage = () => {
+  return <div>
+    <main className={styles.main}>
+      <div className={styles.title}>
+        <h1>Jouer !</h1>
+      </div>
+    </main>
+  </div>
+}
+
+export default Play;
