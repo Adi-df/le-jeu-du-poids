0e20c86bccbf7bfbd706cce2449b46ef9ad58ab7
Adi-df <adidf-web@laposte.net>
2022-04-03 15:40:03 +0200

    Style weights

--- pages/play/weights.tsx
+++ pages/play/weights.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next"
 import PlayerWeightInput from "../../components/PlayerWeightInput"
 import { useStore } from "../../store/store"
+import styles from "../../styles/Weights.module.css"
 
 const Round: NextPage = () => {
   const { players } = useStore(({ players }) => ({ players }))
-  return <div>
-    <div>
+  return <div className={styles.weights}>
+    <div className={styles.list}>
       {players.map(({ id }) => <PlayerWeightInput id={id} />)}
     </div>
   </div>
