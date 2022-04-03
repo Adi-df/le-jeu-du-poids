5645fa87d1ca25b62b316d470ad3c21111cf2f61
Adi-df <adidf-web@laposte.net>
2022-04-03 11:49:15 +0200

    Add delete button

--- components/PlayerInput.tsx
+++ components/PlayerInput.tsx
@@ -1,10 +1,11 @@
 import { FC, useState } from "react"
 
-const PlayerInput: FC<{ name: string }> = ({ name }) => {
+const PlayerInput: FC<{ name: string, onDelete: () => void }> = ({ name, onDelete }) => {
   const [nameInput, setNameInput] = useState<string>(name);
 
   return <div>
     <input type={"text"} value={nameInput} onChange={(e) => setNameInput(e.target.value)} />
+    <button onClick={() => onDelete()}>Delete</button>
   </div>
 }
 
