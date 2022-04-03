8e2d9372b3631bfb76a699a993832756ca24cc64
Adi-df <adidf-web@laposte.net>
2022-04-03 13:01:39 +0200

    Set players in store

--- store/store.ts
+++ store/store.ts
@@ -1,8 +1,12 @@
 import create from "zustand/react";
 
+type GamePlayer = { id: number, name: string, points: number };
 type GameState = {
   targetWeight: number | null,
-  players: { id: number, name: string, points: number }[],
+  players: GamePlayer[],
+
+  setTargetWeight: (target: number) => void,
+  setPlayers: (players: GamePlayer[]) => void,
   addPlayer: (id: number, name: string) => void
 };
 
@@ -11,6 +15,8 @@ const useStore = create<GameState>(set => ({
   players: [],
 
   setTargetWeight: (target: number) => set(_state => ({ targetWeight: target })),
+  setPlayers: (players: GamePlayer[]) => set(_state => ({ players })),
+
   addPlayer: (id: number, name: string) => set(state => ({
     players: [...state.players, {
       id,
