9f5adffe1bb4b89daf4c21936b22d73f5faa9a57
Adi-df <adidf-web@laposte.net>
2022-04-03 15:19:55 +0200

    Set player weight with input

--- components/PlayerWeightInput.tsx
+++ components/PlayerWeightInput.tsx
@@ -2,9 +2,12 @@ import { FC } from "react"
 import { useStore } from "../store/store"
 
 const PlayerWeightInput: FC<{ id: number }> = ({ id }) => {
-  const { getPlayer } = useStore(({ getPlayer }) => ({ getPlayer }))
+  const { getPlayer, setPlayerWeight } = useStore(({ getPlayer, setPlayerWeight }) => ({ getPlayer, setPlayerWeight }))
+
   return <div>
     <label>{getPlayer(id)?.name}</label>
+    <input type={"number"} min={0} value={getPlayer(id)?.weight} onChange={(e) => setPlayerWeight(id, parseInt(e.target.value))} />
+    <label>g</label>
   </div>
 }
 
