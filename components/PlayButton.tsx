0f8a6d89d655ca71941a845e52329004c14ea9ac
Adi-df <adidf-web@laposte.net>
2022-04-03 10:48:12 +0200

    Fix typo

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -1,11 +1,11 @@
-import {FC} from "react"
+import { FC } from "react"
 import Link from "next/link"
 import styles from "../styles/PlayButton.module.css"
 
 const PlayButton: FC = () => {
   return <div className={styles.button}>
     <Link href="/play">
-      Play !
+      Jouer !
     </Link>
   </div>
 }
