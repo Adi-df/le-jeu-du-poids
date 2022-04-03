ecabe8028a8714a81c78f79735838076aa0a9a10
Adi-df <adidf-web@laposte.net>
2022-04-03 11:58:56 +0200

    Style player name input

--- components/PlayerInput.tsx
+++ components/PlayerInput.tsx
@@ -1,11 +1,12 @@
 import { FC, useState } from "react"
+import styles from "../styles/PlayerInput.module.css"
 
 const PlayerInput: FC<{ name: string, onDelete: () => void }> = ({ name, onDelete }) => {
   const [nameInput, setNameInput] = useState<string>(name);
 
-  return <div>
-    <input type={"text"} value={nameInput} onChange={(e) => setNameInput(e.target.value)} />
-    <button onClick={() => onDelete()}>Delete</button>
+  return <div className={styles.input}>
+    <input className={styles.name} type={"text"} value={nameInput} onChange={(e) => setNameInput(e.target.value)} />
+    <button className={styles.delete} onClick={() => onDelete()}>Delete</button>
   </div>
 }
 
