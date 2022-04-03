4cf83e731b5a26bfd7cf4d6305ccada83c6ed4cb
Adi-df <adidf-web@laposte.net>
2022-04-03 14:50:41 +0200

    Display rounds and target weight

--- pages/play/round.tsx
+++ pages/play/round.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next"
+import { useEffect } from "react"
+import { useStore } from "../../store/store"
+
+const MIN_WEIGHT = 100;
+const MAX_WEIGHT = 4000;
 
 const Round: NextPage = () => {
-  return <div></div>
+  const { leftRounds, targetWeight, setTargetWeight } = useStore(({ leftRounds, targetWeight, setTargetWeight }) => ({ leftRounds, targetWeight, setTargetWeight }))
+
+  useEffect(() => setTargetWeight(Math.floor(Math.random() * (MAX_WEIGHT - MIN_WEIGHT) + MIN_WEIGHT)), [])
+
+  return <div>
+    <h2>{leftRounds} tours restants</h2>
+    <h1>{targetWeight}g</h1>
+  </div>
 }
 
 export default Round
