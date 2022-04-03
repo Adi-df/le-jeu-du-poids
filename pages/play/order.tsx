c4cfd62a844d804ef848660b6f7eb0fcd5f752f4
Adi-df <adidf-web@laposte.net>
2022-04-03 16:15:17 +0200

    Add next round/end button

--- pages/play/order.tsx
+++ pages/play/order.tsx
@@ -1,20 +1,24 @@
 import type { NextPage } from "next"
+import { useEffect } from "react"
 import { useStore } from "../../store/store"
 import styles from "../../styles/Order.module.css"
 
 const Round: NextPage = () => {
-  const { players, targetWeight } = useStore(({ players, targetWeight }) => ({ players, targetWeight }))
+  const { players, targetWeight, leftRounds, decrementRounds } = useStore(({ players, targetWeight, leftRounds, decrementRounds }) => ({ players, targetWeight, leftRounds, decrementRounds }))
+
+  useEffect(() => decrementRounds(), [])
 
   const sortedPlayers = players.map(player => ({ dist: Math.abs((targetWeight || 0) - player.weight), player })).sort((p1, p2) => p1.dist - p2.dist).map(({ player }) => player);
 
   return <div className={styles.order}>
-    <div>
+    <div className={styles.players}>
       {sortedPlayers.map((player, i) => <div className={styles.player}>
         <div className={styles.place}>{i + 1}</div>
         <div className={styles.name}>{player.name}</div>
         <div className={styles.points}>+{sortedPlayers.length - i}</div>
       </div>)}
     </div>
+    <button>{leftRounds > 0 ? "Tour suivant" : "Résultats"}</button>
   </div>
 }
 
