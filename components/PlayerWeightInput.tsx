611766640b614e7a05a77c4d3786003a4320a2c8
Adi-df <adidf-web@laposte.net>
2022-04-03 15:28:10 +0200

    Style weight input

--- components/PlayerWeightInput.tsx
+++ components/PlayerWeightInput.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react"
 import { useStore } from "../store/store"
+import styles from "../styles/PlayerWeightInput.module.css"
 
 const PlayerWeightInput: FC<{ id: number }> = ({ id }) => {
   const { getPlayer, setPlayerWeight } = useStore(({ getPlayer, setPlayerWeight }) => ({ getPlayer, setPlayerWeight }))
 
-  return <div>
-    <label>{getPlayer(id)?.name}</label>
-    <input type={"number"} min={0} value={getPlayer(id)?.weight} onChange={(e) => setPlayerWeight(id, parseInt(e.target.value))} />
-    <label>g</label>
+  return <div className={styles.weight}>
+    <label className={styles.name}>{getPlayer(id)?.name}</label>
+    <input className={styles.input} type={"number"} min={0} value={getPlayer(id)?.weight} onChange={(e) => setPlayerWeight(id, parseInt(e.target.value))} />
+    <label className={styles.suffix}>g</label>
   </div>
 }
 
