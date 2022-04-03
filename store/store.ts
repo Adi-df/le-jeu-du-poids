02c6c216633452bd0e02ffd0dd958f68a5620ab7
Adi-df <adidf-web@laposte.net>
2022-04-03 15:18:02 +0200

    Set weight in store

--- store/store.ts
+++ store/store.ts
@@ -1,6 +1,6 @@
 import create from "zustand";
 
-type GamePlayer = { id: number, name: string, points: number };
+type GamePlayer = { id: number, name: string, points: number, weight: number };
 type GameState = {
   targetWeight: number | null,
   players: GamePlayer[],
@@ -12,6 +12,7 @@ type GameState = {
   getPlayer: (id: number) => GamePlayer | undefined,
   setPlayer: (id: number, player: GamePlayer) => void,
   setPlayerName: (id: number, name: string) => void,
+  setPlayerWeight: (id: number, weight: number) => void,
   removePlayer: (id: number) => void,
   addPlayer: (id: number, name: string) => void,
   decrementRounds: () => void
@@ -30,13 +31,15 @@ const useStore = create<GameState>((set, get) => ({
     players: [...state.players, {
       id,
       name,
-      points: 0
+      points: 0,
+      weight: 0,
     }]
   })),
   removePlayer: (id: number) => set(({ players }) => ({ players: players.filter(({ id: pid }) => id !== pid) })),
   getPlayer: (id: number) => get().players.find(({ id: pid }) => pid == id),
   setPlayer: (id: number, player: GamePlayer) => set(({ players }) => ({ players: players.map((p) => id === p.id ? player : p) })),
-  setPlayerName: (id: number, name: string) => set(({ players }) => ({ players: players.map((p) => ({ name: id === p.id ? name : p.name, id: p.id, points: p.points })) })),
+  setPlayerName: (id: number, name: string) => set(({ players }) => ({ players: players.map((p) => ({ name: id === p.id ? name : p.name, id: p.id, points: p.points, weight: p.weight })) })),
+  setPlayerWeight: (id: number, weight: number) => set(({ players }) => ({ players: players.map((p) => ({ weight: id === p.id ? weight : p.weight, id: p.id, points: p.points, name: p.name })) })),
   decrementRounds: () => set(({ leftRounds }) => ({ leftRounds: leftRounds - 1 }))
 }));
 
