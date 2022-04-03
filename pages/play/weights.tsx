f8ade51b8b2b6021baf7fbce364c2e59ce55bff5
Adi-df <adidf-web@laposte.net>
2022-04-03 15:45:36 +0200

    Add Link to /play/order

--- pages/play/weights.tsx
+++ pages/play/weights.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next"
+import Link from "next/link"
 import PlayerWeightInput from "../../components/PlayerWeightInput"
 import { useStore } from "../../store/store"
 import styles from "../../styles/Weights.module.css"
@@ -9,6 +10,7 @@ const Round: NextPage = () => {
     <div className={styles.list}>
       {players.map(({ id }) => <PlayerWeightInput id={id} />)}
     </div>
+    <Link href="/play/order"><button className={styles.results}>Resultats</button></Link>
   </div>
 }
 
